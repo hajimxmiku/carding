072a52774efce9e0d6b0875ae234267f8a4f7fb4
hajimxmiku <anakhandsomee@gmail.com>
2017-12-29 20:59:10 +0800

    Update miku.sh

--- miku.sh
+++ miku.sh
@@ -103,7 +103,7 @@ rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 wget -O /etc/nginx/nginx.conf "http://files.rzvpn.net/rz/nginx.conf"
 mkdir -p /home/vps/public_html
-echo "<pre>Setup by meow | telegram t.me/systemless_root</pre>" > /home/vps/public_html/index.php
+echo "<pre>Setup by miku | telegram t.me/systemless_root</pre>" > /home/vps/public_html/index.php
 echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
 wget -O /etc/nginx/conf.d/vps.conf "http://files.rzvpn.net/rz/vps.conf"
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
